452c0449b55e9005022f5513e9b1d1008ee0a6dc
mamun <mamun@muhammeds-MacBook-Air.local>
2024-10-04 22:38:39 +0600

    wow

--- global.js
+++ global.js
@@ -1,3 +1,13 @@
-crn(`
-<v bgo clzzz c p15 ttu>wow</v>    
-`)
+import {titled, counter} from './manup.js'
+import {titled as aaa, counter as ccc} from './manu.js'
+
+titled.ih = `<h cliz f50 bsm5 px15 c5>wow</h>`
+aaa.ih = `<h clez f50 bsm5 px15 c5>aaa</h>`
+
+
+counter.c = ()=> {
+    counter.it = (counter.it*1)+1
+}
+ccc.c = ()=> {
+    ccc.it = (ccc.it*1)+2
+}
---
+++ manu.js
@@ -0,0 +1,8 @@
+const {titled, counter} = crn(`
+    <v bgo clzzz c p15 ttu>
+        <h ref=titled>title</h>
+        <h ref=counter bgw p15 m15 c5>1</h>
+    </v>    
+    `)
+    titled.tc = 12345
+    export  {titled, counter};
